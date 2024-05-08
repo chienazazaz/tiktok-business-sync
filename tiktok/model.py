0cea510ab07617533fdb838cd9568ac2b7b4ef68
Chien Le <lchien063@gmail.com>
2024-05-08 16:21:27 +0700

    fix: disable hourly model

--- tiktok/model.py
+++ tiktok/model.py
@@ -13,18 +13,26 @@ class Model:
     # def transform(self, data: List[Dict]) -> List[Dict]:
     #     return {"data": data}
 
-    def get(self, param: Optional[Dict], request_data: Optional[Dict]=None) -> List[Dict]:
+    def get(
+        self, param: Optional[Dict], request_data: Optional[Dict] = None
+    ) -> List[Dict]:
         data = []
         param["page_size"] = param.get("page_size") or 50
 
-        def _get(param: Optional[Dict], request_data: Optional[Dict]=None) -> List[Dict]:
-            response: Dict = TiktokClient().get(path=self.path, param=param, data=request_data).get("data")
-            # print(response)
-            # response = response_.get("data")
+        def _get(
+            param: Optional[Dict], request_data: Optional[Dict] = None
+        ) -> List[Dict]:
+            response: Dict = (
+                TiktokClient()
+                .get(path=self.path, param=param, data=request_data)
+                .get("data")
+            )
             result, page_info = response.get("list"), response.get("page_info")
             data.extend(result) if result else None
             if page_info.get("page") < page_info.get("total_page"):
-                return _get({**param, "page": page_info.get("page") + 1}, request_data={})
+                return _get(
+                    {**param, "page": page_info.get("page") + 1}, request_data={}
+                )
             else:
                 return data
 
